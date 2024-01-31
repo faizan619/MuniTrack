ff145abad424a4ff8190c3236ed59c3faeca6497
faizan619 <alamf6023@gmail.com>
2024-01-31 14:56:44 +0530

    accound layout done

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,21 +1,40 @@
-import details from "./accountInfo"
-export default function Page(){
-    console.log(details)
-    console.log(details[1].name)
-    return(
-        <div className="px-3 flex flex-col justify-between min-h-[30rem] ">
-            <div>
-                {
-                    details.map((item)=>(
-                        <div key={item.key} className="flex border border-black border-b-white py-3 justify-between text-lg uppercase">
-                            <p>{item.logo}</p>
-                            <p>{item.name}</p>
-                            <p></p>
-                        </div>
-                    ))
-                }
-            </div>
-            <button className="border w-full rounded-md py-2 hover:bg-green-600 font-bold transition-all">Logout</button>
+export default function Page() {
+  return (
+    <div className="px-3 flex flex-col justify-between min-h-[30rem]  ">
+      <div className="flex-1  flex flex-col justify-evenly items-center">
+        <div className="flex py-2 px-2 border border-black hover:border-b-green-600 cursor-pointer hover:text-green-600 justify-between text-lg uppercase">
+          <p></p>
+          <p>Profile</p>
+          <p></p>
         </div>
-    )
+
+        <div className="flex py-2 px-2 border border-black hover:border-b-green-600 cursor-pointer hover:text-green-600 justify-between text-lg uppercase">
+          <p></p>
+          <p>View issues</p>
+          <p></p>
+        </div>
+
+        <div className="flex py-2 px-2 border border-black hover:border-b-green-600 cursor-pointer hover:text-green-600 justify-between text-lg uppercase">
+          <p></p>
+          <p>Community Chat</p>
+          <p></p>
+        </div>
+        <div className="flex py-2 px-2 border border-black hover:border-b-green-600 cursor-pointer hover:text-green-600 justify-between text-lg uppercase">
+          <p></p>
+          <p>Report</p>
+          <p></p>
+        </div>
+        <div className="flex py-2 px-2 border border-black hover:border-b-green-600 cursor-pointer hover:text-green-600 justify-between text-lg uppercase">
+          <p></p>
+          <p>Terms and Condition</p>
+          <p></p>
+        </div>
+
+      </div>
+
+      <button className="border w-full rounded-md py-2 hover:bg-green-600 text-xl font-bold transition-all">
+        Logout
+      </button>
+    </div>
+  );
 }
