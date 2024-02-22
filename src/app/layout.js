bb8e3992c45296e55ed24fffc67b46af082195da
faizan619 <alamf6023@gmail.com>
2024-02-23 00:18:45 +0530

    added toaster

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Toaster
+        position='top-center'
+          toastOptions={{
+            success:{
+              theme:{
+                primary:'#4aed88'
+              }
+            }
+          }}
+      />
         <Header/>
         <Navbar />
         <div>{children}</div>
