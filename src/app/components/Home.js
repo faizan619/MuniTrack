0382ae752d384846e38476783dab50e5cf1edb24
faizan619 <alamf6023@gmail.com>
2024-02-22 16:56:55 +0530

    minor changes

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -6,37 +6,38 @@ import { useEffect } from "react";
 import Button from "./Button";
 
 export default function Home() {
-  try {
-    const { user } = useAuthContext();
-    const router = useRouter();
+  const { user } = useAuthContext(); // Moved to top level
+  const router = useRouter(); // Moved to top level
+  const auth = getAuth(); // Moved to top level
 
-    useEffect(() => {
-      if (user == null) router.push("/register");
-    }, [user]);
+  useEffect(() => {
+    if (user == null) router.push("/register");
+  }, [user, router]); // Added router to the dependency array
 
-    const auth = getAuth();
-    const LogoutBtn = () => {
-      signOut(auth)
-        .then(() => {
-          alert("Sign out successfully");
-          router.push("/");
-        })
-        .catch((error) => {
-          alert("Problem in Signing out");
-        });
-    };
-    return (
-      <main className="p-2 flex flex-col gap-2 min-h-[35rem]  sm:bg-yellow-600 md:bg-blue-700 lg:bg-green-700 xl:bg-red-700">
-        <div className="flex justify-between items-center">
-          <p className="text-xl font-extrabold uppercase">Issue</p>
-          <p>Search</p>
-        </div>
-        <div className="bg-green-600 rounded-lg p-3 h-full flex-1">
-          <Button title="api/issue" url="/api/issue" />
-        </div>
-      </main>
-    );
-  } catch (e) {
+  const LogoutBtn = () => {
+    signOut(auth)
+      .then(() => {
+        alert("Sign out successfully");
+        router.push("/");
+      })
+      .catch((error) => {
+        alert("Problem in Signing out");
+      });
+  };
+
+  if (!user) { // Conditional rendering
     return <div>Only Logined Users can view this page</div>;
   }
+
+  return (
+    <main className="p-2 flex flex-col gap-2 min-h-[35rem]  sm:bg-yellow-600 md:bg-blue-700 lg:bg-green-700 xl:bg-red-700">
+      <div className="flex justify-between items-center">
+        <p className="text-xl font-extrabold uppercase">Issue</p>
+        <p>Search</p>
+      </div>
+      <div className="bg-green-600 rounded-lg p-3 h-full flex-1">
+        <Button title="api/issue" url="/api/issue" />
+      </div>
+    </main>
+  );
 }
