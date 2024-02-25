199fbf022c33403913372ef98bdc9bc2e6eb9933
faizan619 <alamf6023@gmail.com>
2024-02-25 15:15:03 +0530

    complete with chat app

--- src/app/chat/Chat.jsx
+++ src/app/chat/Chat.jsx
@@ -1,14 +1,47 @@
 "use client";
 import { useAuthContext } from "@/context/AuthContext";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function Chat() {
   const { user } = useAuthContext(); // Moved to top level
   const router = useRouter(); // Moved to top level
   const auth = getAuth(); // Moved to top level
+  const [msg,setMsg] = useState("");  //for taking msg input
+
+  const userName = auth.currentUser?.email;
+
+  const submitData = async (e)=>{
+    e.preventDefault();
+    if(msg){
+      const res = await fetch("https://munitrack-1-default-rtdb.firebaseio.com/chat.json",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json",
+        },
+        body:JSON.stringify({
+          msg,
+          userName
+        }),
+      });
+      // console.log("response here :",res);
+      if(res){
+        toast.remove();
+        toast.success("Message Sent!");
+        setMsg("");
+      }
+      else{
+        toast.remove();
+        toast.error("Network Issue. Please Refresh!")
+      }
+    }
+    else{
+      toast.remove();
+      toast.error("Enter Message!")
+    }
+  }
 
   useEffect(() => {
     if (user == null) {
@@ -16,104 +49,71 @@ export default function Chat() {
       toast.error("Please login to continue!");
       router.push("/login");
     }
-  }, [user, router]); // Added router to the dependency array
+  }, [user, router]); 
 
   if (!user) {
-    // Conditional rendering
     return <div>Only Logined Users can view this page</div>;
   }
   return (
     <div className="h-[91.5vh] bg-green-400 p-1">
-      <div className="h-4/5 ">
+      <div className="h-[85%] md:h-full ">
         <div className="flex flex-col-reverse gap-3 h-full overflow-auto px-2">
-          <div className="flex justify-end">
-            <div className="border flex flex-col max-w-[70%] text-gray-800 bg-white rounded-md">
-              <h1 className="text-sm inline-block bg-gray-200 p-1">
-                {auth.currentUser.email}
-              </h1>
-              <p className="inline-block text-lg rounded-md p-1">Hello</p>
-            </div>
-          </div>
-
-          <div className="flex justify-start">
-            <div className="flex flex-col max-w-[70%] text-gray-800 bg-white rounded-md overflow-hidden">
-              <h1 className="text-sm inline-block bg-gray-200 p-1">
-                {auth.currentUser.email}
-              </h1>
-              <p className="inline-block text-lg bg-green-700 text-white p-1">
-                Hello1
-              </p>
-            </div>
-          </div>
-
-          <div className="flex justify-end">
-            <div className="border flex flex-col max-w-[70%] text-gray-800 bg-white rounded-md">
-              <h1 className="text-sm inline-block bg-gray-200 p-1">
-                {auth.currentUser.email}
-              </h1>
-              <p className="inline-block text-lg rounded-md p-1">
-                or kaisa hai
-              </p>
-            </div>
-          </div>
-
-          <div className="flex justify-start">
-            <div className="flex flex-col max-w-[70%] text-gray-800 bg-white rounded-md overflow-hidden">
-              <h1 className="text-sm inline-block bg-gray-200 p-1">
-                {auth.currentUser.email}
-              </h1>
-              <p className="inline-block text-lg bg-green-700 text-white p-1">
-                mast hu yarr! tu bata kaha tha itne dino sai . dost ko bhul gya
-                kya
-              </p>
-            </div>
-          </div>
-
-          <div className="flex justify-end">
-            <div className="border flex flex-col max-w-[70%] text-gray-800 bg-white rounded-md">
-              <h1 className="text-sm inline-block bg-gray-200 p-1">
-                {auth.currentUser.email}
-              </h1>
-              <p className="inline-block text-lg rounded-md p-1">
-                chup snake! kal he toh baat kya.! short term memory hai kya bai
-                teko jo bhul jata hai.
-              </p>
-            </div>
-          </div>
-
-          <div className="flex justify-start">
-            <div className="flex flex-col max-w-[70%] text-gray-800 bg-white rounded-md overflow-hidden">
-              <h1 className="text-sm inline-block bg-gray-200 p-1">
-                {auth.currentUser.email}
-              </h1>
-              <p className="inline-block text-lg bg-green-700 text-white p-1">
-                testing k liye likhra hu bai. tu serior kun hora chill karna .
-              </p>
-            </div>
-          </div>
-
-          <div className="flex justify-end">
-            <div className="border flex flex-col max-w-[70%] text-gray-800 bg-white rounded-md">
-              <h1 className="text-sm inline-block bg-gray-200 p-1">
-                {auth.currentUser.email}
-              </h1>
-              <p className="inline-block text-lg rounded-md p-1">
-                fir theek hia! meko laga firse tere paga giri chalu ho gye!
-              </p>
-            </div>
-          </div>
-        </div>
-        <form className="flex gap-2 pt-2 w-full">
+        <form className="flex gap-2 pt-2 w-full" onSubmit={submitData}>
           <input
             type="text"
             placeholder="Enter Message... "
-            className="w-full px-2 py-1 text-lg rounded-md"
+            className="w-full px-2 py-1 text-lg rounded-md text-black"
+            value={msg}
+            onChange={(e)=>setMsg(e.target.value)}
           />
-          <button className="border px-5 text-white hover:bg-white hover:text-black">
+          <button type="submit" className="border px-5 text-white hover:bg-white hover:text-black bg-green-900">
             Sent
           </button>
         </form>
+          <Chatting msg={msg} />
+        </div>
       </div>
     </div>
   );
+}
+
+
+const Chatting = ({msg})=>{
+  const [details, setDetails] = useState(null);
+  const [error, setError] = useState(null);
+  const auth = getAuth(); // Moved to top level
+  const userName = auth.currentUser.email;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://munitrack-1-default-rtdb.firebaseio.com/chat.json",
+          { cache: "no-cache" }
+        );
+        const data = await response.json();
+        setDetails(data);
+      } catch (error) {
+        setError("Error Fetching the Chats.");
+      }
+    };
+    fetchData();
+    console.log("fetching...")
+  },[msg]);
+
+  if (error) return <div>Error Fetching the Chats.</div>;
+  if (!details) return <div>Loading...</div>;
+  return(
+    <div className="flex flex-col gap-3">
+      {Object.values(details).map((chat, index) => (
+        <div key={index} className={`flex ${chat.userName === userName?"justify-end":"justify-start"}`}>
+          <div className="border flex flex-col max-w-[70%] text-gray-800 bg-white rounded-md overflow-hidden">
+            <h1 className="text-sm inline-block bg-gray-300 p-1">
+              {chat.userName}
+            </h1>
+            <p className={`inline-block text-lg ${chat.userName === userName?"bg-white":"bg-green-700 text-white"}  p-1`}>{chat.msg}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
