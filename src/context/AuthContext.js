70cdc5ccddacbdf0a4700caf0f7865f92d0f0234
faizan619 <alamf6023@gmail.com>
2024-02-26 23:53:19 +0530

    minor auth changed

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -16,6 +16,7 @@ export const AuthContextProvider = ({
         const unsubscribe = onAuthStateChanged(auth,(user)=>{
             if(user){
                 setUser(user);
+                console.log("User at AuthContext : ",user)
             }
             else{
                 setUser(null);
