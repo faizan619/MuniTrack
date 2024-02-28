afb67d81a4b8a42a527cd50c55c1ee9d175a3596
faizan619 <alamf6023@gmail.com>
2024-02-28 23:51:11 +0530

    create remaining pages

---
+++ src/app/search/page.js
@@ -0,0 +1,8 @@
+export default function Page(){
+    return(
+        <div className="h-[91.5vh] wallpaper p-5 text-white text-center">
+            <h1 className="border p-5 rounded-lg mb-10 ">Search Page</h1>
+            <p>This Page is Still Left for building</p>
+        </div>
+    )
+}
