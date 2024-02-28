c5e3503c95b2fa5b8f29e53363629e9c7f374134
faizan619 <alamf6023@gmail.com>
2024-02-28 16:54:51 +0530

    finish account page with minor google update

--- src/app/account/Account.jsx
+++ src/app/account/Account.jsx
@@ -1,8 +1,9 @@
 "use client";
 import { useAuthContext } from "@/context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function Account() {
@@ -10,13 +11,16 @@ export default function Account() {
   const router = useRouter();
   const auth = getAuth();
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleIsOpen = () => setIsOpen(!isOpen);
+
   useEffect(() => {
     if (user == null) {
       toast.remove();
       toast.error("Please login to continue!");
       router.push("/google");
     }
-
   }, [user, router]);
 
   const LogoutBtn = () => {
@@ -39,28 +43,98 @@ export default function Account() {
   }
   return (
     <div className="min-h-[91.5vh] p-3 flex md:justify-center pt-5">
-      <div className="flex flex-col gap-5 w-full md:w-3/4">
-        <p className=" rounded-md px-3 py-5 text-sm bg-gray-600 text-green-400  ">
-          Name :
-          <span className="uppercase font-bold text-white">
-            {auth.currentUser.displayName}
-          </span>
-        </p>
-        <p
-          className="rounded-md bg-gray-600 px-3 py-5 text-sm text-green-400 hover:bg-gray-700 "
+      <div className="flex flex-col gap-5 w-full md:w-3/4 pb-20">
+        <div className="flex gap-5 items-center flex-col sm:flex-row">
+          <p className=" rounded-md text-sm flex justify-center items-center">
+            <Image
+              src={auth.currentUser.photoURL}
+              height={100}
+              width={100}
+              alt="profile photo"
+              className="rounded-full sm:rounded-md h-52 w-52 sm:h-40 sm:w-60 object-cover"
+            />
+          </p>
+          <div className="w-full flex flex-col gap-5">
+            <p className="rounded-md px-3 py-5 text-sm bg-gray-600 text-green-400 overflow-auto">
+              Name :
+              <span className="sm:ml-4 uppercase font-bold text-white">
+                {auth.currentUser.displayName}
+              </span>
+            </p>
+            <p className="rounded-md bg-gray-600 px-3 py-5 text-sm text-green-400 hover:bg-gray-700 overflow-auto">
+              Email :
+              <span className="sm:ml-4 uppercase font-bold text-white cursor-pointer hover:underline ">
+                {auth.currentUser.email}
+              </span>
+            </p>
+          </div>
+        </div>
+        <div
+          className="bg-gray-600 px-3 py-5 rounded-md text-white flex items-center justify-between hover:underline hover:text-green-500 "
+          onClick={toggleIsOpen}
         >
-          Email :{" "}
-          <span className="uppercase font-bold text-white cursor-pointer hover:underline ">
-            {auth.currentUser.email}
-          </span>
-        </p>
-        <p className="rounded-md bg-gray-600 px-3 py-5 text-sm font-bold uppercase text-white hover:bg-gray-700 cursor-pointer" onClick={()=>{router.push('/account/TermAndCondition')}}>
+          <p className="font-semibold ">View Other Data.</p>
+          <svg
+            data-accordion-icon
+            class="w-3 h-3 rotate-180 shrink-0"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5 5 1 1 5"
+            />
+          </svg>
+        </div>
+        {isOpen && (
+          <div className="flex flex-col gap-1 -mt-4">
+            <p className="rounded-md bg-green-500 px-3 py-5 text-sm text-gray-900 hover:bg-green-600 overflow-auto">
+              Account Created :
+              <span className="uppercase font-bold text-white cursor-pointer hover:underline sm:ml-4 ">
+                {auth.currentUser.metadata.creationTime}
+              </span>
+            </p>
+            <p className="rounded-md bg-green-500 px-3 py-5 text-sm text-gray-900 hover:bg-green-600 overflow-auto">
+              Last SignIn :
+              <span className="sm:ml-4 uppercase font-bold text-white cursor-pointer hover:underline ">
+                {auth.currentUser.metadata.lastSignInTime}
+              </span>
+            </p>
+            <p className="rounded-md bg-green-500 px-3 py-5 text-sm text-gray-900 hover:bg-green-600 overflow-auto">
+              Unique ID :
+              <span className="sm:ml-4 uppercase font-bold text-white cursor-pointer hover:underline ">
+                {auth.currentUser.uid}
+              </span>
+            </p>
+          </div>
+        )}
+        <p
+          className="rounded-md bg-gray-600 px-3 py-5 text-sm font-bold uppercase text-white hover:bg-gray-700 cursor-pointer"
+          onClick={() => {
+            router.push("/account/TermAndCondition");
+          }}
+        >
           Terms And Conditions.
         </p>
-        <p className="rounded-md bg-gray-600 px-3 py-5 text-sm font-bold uppercase text-white hover:bg-gray-700 cursor-pointer" onClick={()=>{router.push('/account/DeveloperDetails')}}>
+        <p
+          className="rounded-md bg-gray-600 px-3 py-5 text-sm font-bold uppercase text-white hover:bg-gray-700 cursor-pointer"
+          onClick={() => {
+            router.push("/account/DeveloperDetails");
+          }}
+        >
           Developer Details
         </p>
-        <p className="rounded-md bg-gray-600 px-3 py-5 text-sm font-bold uppercase text-white hover:bg-gray-700 cursor-pointer" onClick={()=>{router.push('/account/Feedback')}}>
+        <p
+          className="rounded-md bg-gray-600 px-3 py-5 text-sm font-bold uppercase text-white hover:bg-gray-700 cursor-pointer"
+          onClick={() => {
+            router.push("/account/Feedback");
+          }}
+        >
           How was our app ?
         </p>
         <button
--- src/app/google/Google.jsx
+++ src/app/google/Google.jsx
@@ -7,14 +7,14 @@ import { useAuthContext } from "@/context/AuthContext";
 
 export default function Google() {
   const router = useRouter();
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
   const auth = getAuth();
   const { user } = useAuthContext();
 
   useEffect(() => {
     if (user != null) {
       toast.remove();
-      toast.success("You are Already login!");
+      toast.success(`Welcome ${auth.currentUser.displayName}`);
       router.push("/");
     }
   }, [user, router]);
@@ -53,6 +53,7 @@ export default function Google() {
         aria-label="Login with Google"
         type="button"
         className="border border-white flex items-center gap-3 font-bold text-xl px-7 py-5 rounded-md bg-white text-black"
+        disabled={loading}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -61,7 +62,7 @@ export default function Google() {
         >
           <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
         </svg>
-        <p>Login with Google ID</p>
+        <p>{loading?"Loading...":"Login with Google"}</p>
       </button>
     </div>
     <div className="inline-block lg:hidden">
@@ -70,6 +71,7 @@ export default function Google() {
         aria-label="Login with Google"
         type="button"
         className="border-white flex items-center gap-3 font-bold text-xl px-7 py-5 rounded-md bg-white text-black"
+        disabled={loading}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -78,7 +80,7 @@ export default function Google() {
         >
           <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
         </svg>
-        <p>Login with Google</p>
+        <p>{loading?"Loading...":"Login with Google"}</p>
       </button>
     </div>
     </div>
