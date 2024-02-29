89a3a74414a8a95160821f60d8e5168f152d2117
faizan619 <alamf6023@gmail.com>
2024-02-29 13:24:41 +0530

    added add button

---
+++ src/app/element/component/AddButton.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import AnimeIcon from "../../../../public/assets/addicon.png";
+import Link from "next/link";
+export default function AddButton() {
+  return (
+    <Link href={"/add"} className="absolute bottom-6 right-6">
+      <Image
+        src={AnimeIcon}
+        height={70}
+        width={70}
+        className="cursor-pointer hover:scale-105 transition-all"
+      />
+    </Link>
+  );
+}
