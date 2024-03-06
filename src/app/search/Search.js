ca8425a9e7bf00a6edb80d16f4c9973eb8c0a74a
faizan619 <alamf6023@gmail.com>
2024-03-06 17:08:02 +0530

    start building search page

---
+++ src/app/search/Search.js
@@ -0,0 +1,18 @@
+export default function Search() {
+  return (
+    <div className="h-[90vh] wallpaper text-white ">
+      <div>
+        View  Registered Minister
+      </div>
+      <div>
+        Search Issue
+      </div>
+      <div>
+        View Pending Issues
+      </div>
+      <div>
+        View Solved Issues
+      </div>
+    </div>
+  );
+}
