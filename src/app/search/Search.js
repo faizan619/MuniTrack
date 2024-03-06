1b3bf5237f845b0c34fe28d12092de9fef0aaa47
faizan619 <alamf6023@gmail.com>
2024-03-07 01:15:51 +0530

    building ui for searching issue

--- src/app/search/Search.js
+++ src/app/search/Search.js
@@ -1,18 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Search() {
+  const [inp, setInp] = useState("");
+  const [data,setData] = useState("");
+
+  const [loading,setLoading] = useState(false)
+
+  const getIssue = async ()=>{
+    if(inp!==""){
+      let response = await fetch(`${NEXT_PUBLIC_DOMAIN_URL}/backend/issue/${inp}`);
+      response = await response.json();
+      console.log("response :",response);
+      setData(response);
+    }
+    else{
+      setData([]);
+    }
+  }
+
+  useEffect(()=>{
+    if(inp!==""){
+      getIssue();
+    }
+  },[inp]);
+
   return (
-    <div className="h-[90vh] wallpaper text-white ">
-      <div>
-        View  Registered Minister
-      </div>
-      <div>
-        Search Issue
-      </div>
-      <div>
-        View Pending Issues
-      </div>
-      <div>
-        View Solved Issues
-      </div>
+    <div className="h-[90vh] wallpaper text-white p-3 ">
+      <input
+        type="text"
+        placeholder="Search Issue"
+        className="text-black px-3 py-1 w-full rounded-md"
+        value={inp}
+        onChange={(e) => {
+          setInp(e.target.value());
+        }}
+      />
+      {!inp ? (
+        <>
+          <div>View Incharge</div>
+          <div>View Solved Issues</div>
+        </>
+      ) : (
+        <p></p>
+      )}
     </div>
   );
 }
