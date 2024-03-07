e2be0c247230bd489762d77820c8b7b7c971ae42
faizan619 <alamf6023@gmail.com>
2024-03-07 08:52:35 +0530

    create detail btn

---
+++ src/app/element/component/DetailBtn.jsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+export default function DetailBtn({url}){
+    const router = useRouter();
+    return(
+        <button className="border bg-gray-600 text-white py-1 rounded-md" onClick={()=>{router.push(url)}}>View Details...</button>
+    )
+}
