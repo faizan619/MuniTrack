9856d0a0d36c59651cf89b6899abfa63a4164a4f
faizan619 <alamf6023@gmail.com>
2024-03-20 07:13:24 +0530

    if issue resolved then edit option is removed

--- src/app/issue/[issueid]/Details.jsx
+++ src/app/issue/[issueid]/Details.jsx
@@ -9,7 +9,7 @@ export default function Details({ name }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const {user} = useAuthContext();
+  const { user } = useAuthContext();
 
   const getIssue = async () => {
     setLoading(true);
@@ -57,34 +57,82 @@ export default function Details({ name }) {
               />
               <div className="text-left p-3 rounded-md w-3/4 flex flex-col gap-4 bg-purple-600">
                 <p>
-                  Title : <span className="font-bold uppercase underline">{data.issue_title}</span>
+                  Title :{" "}
+                  <span className="font-bold uppercase underline">
+                    {data.issue_title}
+                  </span>
                 </p>
                 <p>
-                    Info : <span className="capitalize font-bold">{data.issue_describe}</span>
+                  Info :{" "}
+                  <span className="capitalize font-bold">
+                    {data.issue_describe}
+                  </span>
                 </p>
                 <p>
-                    Issue occured At : <span className="font-bold underline">{data.issue_location} {data.issue_manual_location==="none"?"":` or ${data.issue_manual_location}`}</span>
+                  Issue occured At :{" "}
+                  <span className="font-bold underline">
+                    {data.issue_location}{" "}
+                    {data.issue_manual_location === "none"
+                      ? ""
+                      : ` or ${data.issue_manual_location}`}
+                  </span>
                 </p>
                 <p>
-                    Issue Uploaded on : <span className="font-bold">{new Date(data.issue_uploaded_on).toLocaleDateString()}</span>
+                  Issue Uploaded on :{" "}
+                  <span className="font-bold">
+                    {new Date(data.issue_uploaded_on).toLocaleDateString()}
+                  </span>
                 </p>
                 <p>
                   Issue State : <span>{data.issue_state}</span>
                 </p>
+                <p>
+                  Issuer raiser Email : <span>{data.issue_user_email}</span>
+                </p>
+                <p>
+                  You Email : <span>{user.email} </span>
+                </p>
               </div>
-              {
-                data.issue_state==="pending"?(null):(<div>
-                  <Image src={data.issue_resolve_image_url} height={200} width={250} alt="image" />
-                  <p>Issue Resolved By : {data.issue_resolved_on}</p>
-                  <p>Issue Resolved By : {data.issue_resolved_by}</p>
-                </div>)
-              }
+              {data.issue_state === "pending" ? null : (
+                <div>
+                  <Image
+                    src={data.issue_resolve_image_url}
+                    height={200}
+                    width={250}
+                    alt="image"
+                  />
+                  <p>Issue Resolved On : {data.issue_resolved_on}</p>
+                </div>
+              )}
               <div className="flex gap-3">
-
-                {data.issue_state==="pending"?(
-                    user.emailVerified?(<p className="px-5 py-2 rounded-md cursor-pointer transition-all">Issue is Still pending</p>):(<Link href={`/issue/resolved/${data._id}`} className="border px-5 py-2 rounded-md hover:bg-white hover:text-black cursor-pointer transition-all">Resolve This Issue</Link>)
-                ):(<p>Issue Resolved By {data.issue_resolve_by}</p>)}
-                    {user.email === data.issue_user_email && data.state==="pending"?(<a href={`/issue/update/${data._id}`} className="border px-5 py-2 hover:bg-white hover:text-black rounded-md">Edit Your Issue</a>):(null)}
+                {data.issue_state === "pending" ? (
+                  user.emailVerified ? (
+                    <p className="px-5 py-2 rounded-md cursor-pointer transition-all">
+                      Issue is Still pending
+                    </p>
+                  ) : (
+                    <Link
+                      href={`/issue/resolved/${data._id}`}
+                      className="border px-5 py-2 rounded-md hover:bg-white hover:text-black cursor-pointer transition-all"
+                    >
+                      Resolve This Issue
+                    </Link>
+                  )
+                ) : (
+                  <p>Issue Resolved By {data.issue_resolved_by}</p>
+                )}
+                {user.email === data.issue_user_email ? (
+                  data.issue_state === "pending" ? (
+                    <a
+                      href={`/issue/update/${data._id}`}
+                      className="border px-5 py-2 hover:bg-white hover:text-black rounded-md"
+                    >
+                      Edit Your Issue
+                    </a>
+                  ) : (null
+                  )
+                ) : (null
+                )}
               </div>
             </div>
           </>
