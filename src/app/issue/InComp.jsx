a7d0e5b309e39c376ea62de92468b074515afdc4
faizan619 <alamf6023@gmail.com>
2024-03-23 16:25:43 +0530

    added admin list in view section and design the view section of admin

---
+++ src/app/issue/InComp.jsx
@@ -0,0 +1,41 @@
+"use client"
+import { useAuthContext } from "@/context/AuthContext";
+import { kushan,arima,serif } from "../element/fonts";
+export default function InComp({ users }) {
+    const {user} = useAuthContext();
+  return (
+    <div className="text-white px-5 py-3">
+          {users === undefined ? (
+            <p className={`${arima.className}`}>Loading Data..</p>
+          ) : users.length === 0 ? (
+            <p className="text-white">No Data Found</p>
+          ) : (
+            <div className={`flex justify-start flex-wrap ${arima.className}`}>
+              <div>
+                <p className={`${serif.className}`}>Incharge Available : [ {users.length} ]</p>
+                <div className="flex flex-wrap gap-3">
+                  {users.map((item) => (
+                    <div
+                      key={item._id}
+                      className="flex flex-wrap mr-5 gap-5 my-3"
+                    >
+                      <div className={`flex items-center gap-3 p-3 rounded-md ${user.email===item.email?"bg-violet-600 text-white":"bg-white text-black"} select-none`}>
+                        <img
+                          src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
+                            item.displayName
+                          )}&size=35&rounded=true&background=black&color=fff&uppercase=false`}
+                          alt={item.displayName}
+                        />
+                        <p className={`text-xl uppercase font-extrabold  `}>
+                          {item.displayName}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+  );
+}
--- src/app/issue/IssueShow.jsx
+++ src/app/issue/IssueShow.jsx
@@ -1,82 +1,147 @@
-"use client"
+"use client";
 
 import IssueComp1 from "./IssueComp1";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import CampComp from "./CampComp1";
 import { useAuthContext } from "@/context/AuthContext";
+import { arima } from "../element/fonts";
+import InComp from "./InComp";
+import NotAuth from "../element/NotAuth";
 
-export default  function HomePost() {
-  const {user} = useAuthContext();
+export default function HomePost() {
+  const { user } = useAuthContext();
+  const [data, setData] = useState(undefined);
+  const [camp, setCamp] = useState(undefined);
+  const [users, setUsers] = useState(undefined);
+  const [showIssue, SetShowIssue] = useState(true);
+  const [showCamp, SetShowCamp] = useState(false);
+  const [showIn, setIn] = useState(false);
 
-  const [data,setData] = useState(undefined)
-  
+  useEffect(() => {
+    getAllPost();
+  }, []);
+
+  if (!user) {
+    return <NotAuth name={"Chat Room"} />;
+  }
+
   const getAllPost = async () => {
+    toast.remove();
+    toast.loading("Loading the Issue");
     try {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_DOMAIN_URL}/issue`,
-        {cache: "no-store",}
+        { cache: "no-store" }
       );
-      if (!response.ok) {throw new Error(`Fetching Error : Status :${response.status}`);}
+      if (!response.ok) {
+        throw new Error(`Fetching Error : Status :${response.status}`);
+      }
       const raw_issue = await response.json();
-      setData(raw_issue)
+      setData(raw_issue);
+      toast.remove();
     } catch (error) {
       console.error("Fetching issue data failed:", error);
+      toast.error("Some Problem Occus While Fetching! Please Refresh.");
       return { success: false, error };
     }
   };
-  useEffect(()=>{
-    getAllPost();
-  },[data])
-
-  const [camp,setCamp] = useState(undefined)
-  const getAllDrive = async ()=>{
+  const getAllDrive = async () => {
+    toast.remove();
+    toast.loading("Loading the Campaigns");
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN_URL}/campaign`,{
-        cache:"no-store"
-      });
-      if (!response.ok) {throw new Error(`Fetching Error : Status :${response.status}`);}
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_DOMAIN_URL}/campaign`,
+        {
+          cache: "no-store",
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Fetching Error : Status :${response.status}`);
+      }
       const raw_issue = await response.json();
-      setCamp(raw_issue)
+      setCamp(raw_issue);
+      toast.remove();
     } catch (error) {
       console.error("Fetching issue data failed:", error);
-      return { success: false, error };      
+      toast.error("Some Problem Occus While Fetching! Please Refresh.");
+      return { success: false, error };
     }
   };
-
-  useEffect(()=>{
-    getAllDrive();
-  },[camp])
-
-  const [showIssue,SetShowIssue] = useState(true)
-  const handleIssueView = ()=>{
+  const getPost = async () => {
+    toast.remove();
+    toast.loading("Loading the Admin.");
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_DOMAIN_URL}/user`,
+        {
+          cache: "no-store",
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Fetching Error : Status ${response.status}`);
+      }
+      let raw_users = await response.json();
+      setUsers(raw_users);
+      toast.remove();
+    } catch (error) {
+      console.error("Fetching issue data failed:", error);
+      toast.remove();
+      toast.error("Some Problem Occus While Fetching! Please Refresh.");
+      return { success: false, error };
+    }
+  };
+  const handleIssueView = () => {
     SetShowIssue(true);
     SetShowCamp(false);
-  }
-  
-  const [showCamp,SetShowCamp] = useState(false)
-  const handleCampView = ()=>{
+    setIn(false);
+    getAllPost();
+  };
+  const handleCampView = () => {
     SetShowIssue(false);
     SetShowCamp(true);
-  }
+    setIn(false);
+    getAllDrive();
+  };
+  const handleInCharge = () => {
+    SetShowCamp(false);
+    SetShowIssue(false);
+    setIn(true);
+    getPost();
+  };
+  
 
   return (
     <div className="pb-20 wallpaper1 min-h-[90vh]">
-    <div className="flex justify-evenly py-5">
-      <p className={`border text-xl px-5 py-2 rounded-md cursor-pointer hover:scale-105 ${showIssue?"bg-white":""} ${showIssue?"text-black":"text-white"}`}  onClick={handleIssueView}>Issue</p>
-      <p className={`border text-xl px-5 py-2 rounded-md cursor-pointer hover:scale-105 ${showCamp?"bg-white ":""} ${showCamp?"text-black":"text-white"}`}  onClick={handleCampView}>Campaign</p>
-    </div> 
-    {/* {user.emailVerified?(<p>{showIssue && <div className="text-white px-7">
-            <p>Total Issue : [0]</p>
-            <div>
-                <p className="text-center text-lg">This is No Issue Available</p>
-            </div>
-        </div>}</p>):(
-      <p>
-      </p>
-    )} */}
-    {showIssue &&  <IssueComp1 issues={data} />}
-    {showCamp &&  <CampComp camps={camp}/> }
+      <div className={`flex justify-evenly py-5 ${arima.className}`}>
+        <p
+          className={`border text-xl px-5 py-2 rounded-md cursor-pointer hover:scale-105 ${
+            showIssue ? "bg-white" : ""
+          } ${showIssue ? "text-black" : "text-white"}`}
+          onClick={handleIssueView}
+        >
+          Issue
+        </p>
+        <p
+          className={`border text-xl px-5 py-2 rounded-md cursor-pointer hover:scale-105 ${
+            showCamp ? "bg-white " : ""
+          } ${showCamp ? "text-black" : "text-white"}`}
+          onClick={handleCampView}
+        >
+          Campaign
+        </p>
+        <p
+          className={`border text-xl px-5 py-2 rounded-md cursor-pointer hover:scale-105 ${
+            showIn ? "bg-white " : ""
+          } ${showIn ? "text-black" : "text-white"}`}
+          onClick={handleInCharge}
+        >
+          Admin
+        </p>
+      </div>
+      {showIssue && <IssueComp1 issues={data} />}
+      {showCamp && <CampComp camps={camp} />}
+      {showIn && <InComp users={users} />}
     </div>
   );
 }
