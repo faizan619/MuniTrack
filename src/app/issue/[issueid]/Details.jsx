8e5e18bc6a5d085f4de9cd3094c16010419fd268
faizan619 <alamf6023@gmail.com>
2024-03-24 18:43:42 +0530

    designing the issue details page

--- src/app/issue/[issueid]/Details.jsx
+++ src/app/issue/[issueid]/Details.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { arima, car, kushan, serif } from "@/app/element/fonts";
 import { useAuthContext } from "@/context/AuthContext";
 import Image from "next/image";
 import Link from "next/link";
@@ -39,50 +40,121 @@ export default function Details({ name }) {
   }, [name]);
 
   return (
-    <div className="min-h-[90vh] pb-20 text-center wallpaper1 text-white">
+    <div className="min-h-[90vh] flex justify-center pb-20 text-center wallpaper1 text-white">
       {loading ? (
         <p>Loading...</p>
       ) : (
         data && (
-          <>
-            <div className="font-extrabold text-xl py-3">
-              Issue Raised by {data.issue_user_name}
-            </div>
-            <div className="flex flex-col gap-3 justify-center items-center">
+          <div className={`w-[80%] py-5`}>
+            <div
+              className={`flex flex-col border-4 ${
+                data.issue_state === "pending"
+                  ? "border-red-700"
+                  : "border-green-700"
+              } rounded-md px-3 gap-5 py-5 text-left bg-white text-black`}
+            >
+              <p className={`italic text-sm flex justify-between items-center`}>
+              <div className={`text-center capitalize rounded-md`}>
+              {data.issue_state === "pending" ? (
+                  user.emailVerified ? (
+                    null
+                  ) : (
+                    <Link
+                      href={`/issue/resolved/${data._id}`}
+                      className="border px-5 py-2 rounded-md hover:bg-white hover:text-black cursor-pointer transition-all"
+                    >
+                      Resolve This Issue
+                    </Link>
+                  )
+                ) : (
+                  <p className={`capitalize rounded-md py-2 px-3 `}>Issue Resolved By {data.issue_resolved_by} on <span>{data.issue_resolved_on}</span></p>
+                )}
+              </div>
+                <span
+                  className={`capitalize ${
+                    data.issue_state === "pending"
+                      ? "bg-red-700"
+                      : "bg-green-700"
+                  } py-2 px-5 text-white select-none`}
+                >
+                  {data.issue_state}
+                </span>
+              </p>
+              
+              <p
+                className={`${kushan.className} text-center text-3xl capitalize`}
+              >
+                {data.issue_title}
+              </p>
+              <div className={`flex justify-between`}>
+                <div className={`flex gap-2 items-center`}>
+                  <img
+                          src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
+                            data.issue_user_name
+                          )}&size=35&rounded=true&background=black&color=fff&uppercase=false`}
+                          alt={data.issue_user_name}
+                        />
+                  <div className={`${serif.className}`}>
+                    <p>{data.issue_user_name}</p>
+                    <p className={`text-sm`}>
+                      {new Date(data.issue_uploaded_on).toLocaleDateString()}
+                    </p>
+                  </div>
+                </div>
+                <div className={`flex gap-2`}>
+                  <button
+                    className={`${serif.className}`}
+                    onClick={() => {
+                      toast.success("Share Feature is still building.");
+                    }}
+                  >
+                    Share
+                  </button>
+                </div>
+              </div>
               <Image
                 src={data.issue_image_url}
                 height={0}
                 width={300}
-                className="object-contain border w-2/5 rounded-md"
+                className="object-contain border w-full rounded-md border-black"
               />
-              <div className="text-left p-3 rounded-md w-3/4 flex flex-col gap-4 bg-purple-600">
-                <p>
-                  Title :{" "}
-                  <span className="font-bold uppercase underline">
-                    {data.issue_title}
-                  </span>
-                </p>
-                <p>
-                  Info :{" "}
-                  <span className="capitalize font-bold">
-                    {data.issue_describe}
-                  </span>
-                </p>
-                <p>
-                  Issue occured At :{" "}
-                  <span className="font-bold underline">
-                    {data.issue_location}{" "}
-                    {data.issue_manual_location === "none"
-                      ? ""
-                      : ` or ${data.issue_manual_location}`}
-                  </span>
-                </p>
-                <p>
-                  Issue Uploaded on :{" "}
-                  <span className="font-bold">
-                    {new Date(data.issue_uploaded_on).toLocaleDateString()}
-                  </span>
-                </p>
+              <div className={`${serif.className} px-3`}>
+                Location : {data.issue_location}{" "}
+                {data.issue_manual_location === "" ||
+                data.issue_manual_location === "none"
+                  ? ""
+                  : ` or ${data.issue_manual_location}`}
+              </div>
+              <div className={`capitalize ${arima.className} px-3 text-lg`}>
+                {data.issue_describe}
+              </div>
+              <h1 className={`px-3 ${serif.className}`}>Issue Raiser Information</h1>
+              <div className={`bg-gray-800 ${serif.className} text-white p-3 rounded-md`}>
+                <p>Name : {data.issue_user_name}</p>
+                <p>Email : {data.issue_user_email}</p>
+              </div>
+              <div className={`flex justify-center items-center gap-5`}>
+                {user.email === data.issue_user_email ? (
+                  data.issue_state === "pending" ? (
+                    <a
+                      href={`/issue/update/${data._id}`}
+                      className="border px-5 py-2 hover:bg-gray-800 hover:text-white transition-all border-gray-800 rounded-md"
+                    >
+                      Edit Your Issue
+                    </a>
+                  ) : null
+                ) : null}
+              </div>
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+}
+
+/*
+                
                 <p>
                   Issue State : <span>{data.issue_state}</span>
                 </p>
@@ -93,6 +165,7 @@ export default function Details({ name }) {
                   You Email : <span>{user.email} </span>
                 </p>
               </div>
+
               {data.issue_state === "pending" ? null : (
                 <div>
                   <Image
@@ -104,40 +177,5 @@ export default function Details({ name }) {
                   <p>Issue Resolved On : {data.issue_resolved_on}</p>
                 </div>
               )}
-              <div className="flex gap-3">
-                {data.issue_state === "pending" ? (
-                  user.emailVerified ? (
-                    <p className="px-5 py-2 rounded-md cursor-pointer transition-all">
-                      Issue is Still pending
-                    </p>
-                  ) : (
-                    <Link
-                      href={`/issue/resolved/${data._id}`}
-                      className="border px-5 py-2 rounded-md hover:bg-white hover:text-black cursor-pointer transition-all"
-                    >
-                      Resolve This Issue
-                    </Link>
-                  )
-                ) : (
-                  <p>Issue Resolved By {data.issue_resolved_by}</p>
-                )}
-                {user.email === data.issue_user_email ? (
-                  data.issue_state === "pending" ? (
-                    <a
-                      href={`/issue/update/${data._id}`}
-                      className="border px-5 py-2 hover:bg-white hover:text-black rounded-md"
-                    >
-                      Edit Your Issue
-                    </a>
-                  ) : (null
-                  )
-                ) : (null
-                )}
-              </div>
             </div>
-          </>
-        )
-      )}
-    </div>
-  );
-}
+*/
