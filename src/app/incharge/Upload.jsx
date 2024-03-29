1df68126ef36b4561d8da2b889c3781035471a57
faizan619 <alamf6023@gmail.com>
2024-03-29 09:15:12 +0530

    completed the first admin page

--- src/app/incharge/Upload.jsx
+++ src/app/incharge/Upload.jsx
@@ -1,8 +1,10 @@
 "use client";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { arima, serif } from "../element/fonts";
+import Link from "next/link";
 
 export default function Upload() {
   const { user } = useAuthContext();
@@ -20,6 +22,34 @@ export default function Upload() {
     router.push("/incharge/create");
   };
 
+  const [camps, setCamp] = useState(undefined);
+  const getAllDrive = async () => {
+    toast.remove();
+    toast.loading("Loading the Campaigns");
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_DOMAIN_URL}/campaign`,
+        {
+          cache: "no-store",
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Fetching Error : Status :${response.status}`);
+      }
+      const raw_issue = await response.json();
+      setCamp(raw_issue);
+      toast.remove();
+    } catch (error) {
+      console.error("Fetching issue data failed:", error);
+      toast.error("Some Problem Occus While Fetching! Please Refresh.");
+      return { success: false, error };
+    }
+  };
+
+  useEffect(() => {
+    getAllDrive();
+  }, []);
+
   if (!user) {
     return <div>Only Logined Users can view this page</div>;
   } else if (user?.emailVerified) {
@@ -35,13 +65,135 @@ export default function Upload() {
     );
   } else {
     return (
-      <div className="min-h-[90vh] wallpaper1 flex flex-col justify-center items-center gap-5 text-white py-3 px-2">
-        <button
-          className="border uppercase h-60 w-96 text-xl rounded-md hover:bg-white hover:text-black transition-all"
-          onClick={handlecreate}
-        >
-          Create Camp
-        </button>
+      <div className="min-h-[90vh] wallpaper1 text-white py-3 px-2">
+        <div className="flex flex-wrap gap-5 sm:p-3">
+          <button
+            className="uppercase bg-gradient-to-r from-purple-600 to-pink-600 hover:from-pink-600 hover:to-purple-600 hover:bg-gradient-to-t px-5 wallpaper3 w-full sm:w-[25rem] shadow-md shadow-gray-600 py-3 rounded-md transition-all min-h-20 hover:scale-105"
+            onClick={handlecreate}
+          >
+            Host a Campaign
+          </button>
+          {camps === undefined ? (
+            <p className="text-white">Loading Campaign.</p>
+          ) : camps.length === 0 ? (
+            <p>No Campaign Found!</p>
+          ) : (
+            camps.map((item) => (
+              <div
+                key={item._id}
+                className="flex shadow-md text-black shadow-gray-600 justify-center gap-5 px-2 bg-gray-100 rounded-md items-center sm:w-[25rem] "
+              >
+                <div className="py-5 px-1">
+                  <div className="p-2">
+                    <div className="py-2 flex flex-col gap-2">
+                      <p className="italic text-sm">
+                        <span className={``}>
+                          {(() => {
+                            const date = new Date(item.drive_on);
+                            const monthNames = [
+                              "January",
+                              "February",
+                              "March",
+                              "April",
+                              "May",
+                              "June",
+                              "July",
+                              "August",
+                              "September",
+                              "October",
+                              "November",
+                              "December",
+                            ];
+                            const monthName = monthNames[date.getMonth()];
+                            const day = date.getDate();
+
+                            function convertTo12HourFormat(time24) {
+                              const [hour, minute] = time24
+                                .split(":")
+                                .map(Number);
+                              let period = "AM";
+                              let hour12 = hour;
+                              if (hour >= 12) {
+                                period = "PM";
+                                if (hour > 12) {
+                                  hour12 = hour - 12;
+                                }
+                              }
+                              if (hour12 === 0) {
+                                hour12 = 12;
+                              }
+                              return `${hour12
+                                .toString()
+                                .padStart(2, "0")}:${minute
+                                .toString()
+                                .padStart(2, "0")} ${period}`;
+                            }
+
+                            const time = convertTo12HourFormat(item.drive_time);
+                            return `${day} ${monthName} at ${time}`;
+                          })()}
+                        </span>
+                      </p>
+                      <p
+                        className={`relative uppercase inline text-xl font-bold transition-all cursor-pointer before:bg-gray-500  before:absolute before:-bottom-1 before:block before:h-[2px] before:w-full before:origin-bottom-right before:scale-x-0 before:transition before:duration-300 before:ease-in-out hover:before:origin-bottom-left hover:before:scale-x-100`}
+                      >
+                        {item.drive_title} 
+                      </p>
+                    </div>
+                    <p className={`pb-3`}>
+                      Place :{" "}
+                      <span className={`capitalize`}>
+                        {item.drive_location}
+                      </span>
+                    </p>
+                    <p className="text-sm capitalize">
+                      InCharge : {item.drive_host_name}
+                    </p>
+                    <div
+                      className={`flex py-2 gap-2 ${arima.className} justify-between`}
+                    >
+                      <Link
+                        href={item.drive_link}
+                        target="_faizan"
+                        className=" hover:scale-105 transition-all px-6 py-2 border bg-black text-white rounded-md"
+                      >
+                        Join
+                      </Link>
+                      {user.emailVerified ? (
+                        <>
+                          <RWebShare
+                            data={{
+                              text: `Hello! MuniTrack has Organized a ${item.drive_title} at ${item.drive_location}. Wanna Join Us? Here is the Group link`,
+                              url: `${item.drive_link}`,
+                              title: `Let's Together Make the Earth A Better Place to Live :)`,
+                            }}
+                          >
+                            <button
+                              className={`relative hover:scale-105 transition-all  uppercase flex items-center ${serif.className} transition-all cursor-pointer before:bg-gray-500  before:absolute before:-bottom-1 before:block before:h-[2px] before:w-full before:origin-bottom-right before:scale-x-0 before:transition before:duration-300 before:ease-in-out hover:before:origin-bottom-left hover:before:scale-x-100`}
+                            >
+                              Share
+                            </button>
+                          </RWebShare>
+                        </>
+                      ) : (
+                        <>
+                          <button
+                            className=" hover:scale-105 transition-all px-6 py-2 border bg-red-600 text-white rounded-md"
+                            onClick={() => {
+                              handleDelete(item._id);
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
       </div>
     );
   }
--- src/app/incharge/component/SideNav.js
+++ src/app/incharge/component/SideNav.js
@@ -1,21 +1,24 @@
-"use client"
+"use client";
 
+import { arima } from "@/app/element/fonts";
 import { usePathname, useRouter } from "next/navigation";
 
 export default function SideNav() {
-    const router = useRouter();
-    const pathname = usePathname();
-    const isActive = (path) => path === pathname;
+  const router = useRouter();
+  const pathname = usePathname();
+  const isActive = (path) => path === pathname;
   return (
-    <div className={`w-20 h-[90vh] pt-3`}>
+    <div className={`w-20 h-[90vh] bg-gray-800 pt-3`}>
       <div className={`flex flex-col justify-evenly h-full`}>
         <div
           className={`flex justify-center items-center py-3 cursor-pointer hover:scale-110 transition-all featHide`}
-          onClick={()=>{router.push("/incharge")}}
+          onClick={() => {
+            router.push("/incharge");
+          }}
         >
-          <p className={`px-1 rounded-md relative  py-3 ${
-                  isActive("/incharge") ? "border" : ""
-                }`}>
+          <p
+            className={`px-1 rounded-md relative flex flex-col items-center  py-3`}
+          >
             <svg
               className={`w-12 h-8 hover:text-white`}
               fill="#6b7280"
@@ -30,70 +33,61 @@ export default function SideNav() {
               />
             </svg>
             <p
-              className={`text-black px-2 py-1 rounded-md absolute top-2 -right-20 featH bg-white`}
+              className={`text-black px-2 py-1 rounded-md absolute top-5 -right-20 featH bg-white`}
+            >
+              Upload
+            </p>
+            <p
+              className={`text-center text-[12px] ${
+                arima.className
+              } mt-3 uppercase ${
+                isActive("/incharge") ? "text-white" : "text-gray-700"
+              }`}
             >
               Upload
             </p>
           </p>
         </div>
-        {/* <div
-          className={`flex justify-center py-5 items-center cursor-pointer hover:scale-110 transition-all featHide`}
-          onClick={()=>{router.push("/incharge/feedback")}}
-        >
-          <p className={`p-1 rounded-md relative`}>
-            <svg
-              className={` w-12 h-8 `}
-              viewBox="0 0 15 15"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M0 2.5C0 1.67157 0.671573 1 1.5 1H13.5C14.3284 1 15 1.67157 15 2.5V4.5C15 4.88418 14.8556 5.23463 14.618 5.5C14.8556 5.76538 15 6.11582 15 6.5V8.5C15 8.88418 14.8556 9.23462 14.618 9.5C14.8556 9.76538 15 10.1158 15 10.5V12.5C15 13.3284 14.3284 14 13.5 14H1.5C0.671573 14 0 13.3284 0 12.5V10.5C0 10.1158 0.14443 9.76538 0.38195 9.5C0.144428 9.23462 0 8.88418 0 8.5V6.5C0 6.11582 0.144427 5.76538 0.381947 5.5C0.144427 5.23462 0 4.88418 0 4.5V2.5ZM2 4H5V3H2V4ZM5 8H2V7H5V8ZM2 12H5V11H2V12Z"
-                fill="#6b7280"
-              />
-            </svg>
-            <p
-              className={`text-black px-2 py-1 rounded-md absolute top-2 -right-16 featH bg-white`}
-            >
-              Issue
-            </p>
-          </p>
-        </div> */}
-        {/* <div
-          className={`flex justify-center py-5 items-center cursor-pointer hover:scale-110 transition-all featHide`}
-        >
-          <p className={`p-1 rounded-md relative`}>
-            <svg
-              className={` w-12 h-8 `}
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-            >
-              <path
-                d="M22,4.28V15.72a2,2,0,0,1-.77,1.58,2.05,2.05,0,0,1-1.23.42,2,2,0,0,1-.48-.06L10,15.28,8.88,15H7a5,5,0,0,1-3.5-1.43A5,5,0,0,1,7,5H8.88L19.52,2.34a2,2,0,0,1,1.71.36A2,2,0,0,1,22,4.28Z"
-                fill="#6b7280"
-              ></path>
-              <path
-                d="M10,16.31V20a2,2,0,0,1-2,2H6.82a2,2,0,0,1-2-1.61L3.8,15.08a5.68,5.68,0,0,0,1.74.74A5.9,5.9,0,0,0,7,16H8.76Z"
-                fill="#6b7280"
-              ></path>
-            </svg>
-            <p
-              className={`text-black px-2 py-1 rounded-md absolute top-2 -right-24 featH bg-white`}
-            >
-              Campaign
-            </p>
-          </p>
-        </div> */}
         <div
           className={`flex justify-center py-5 items-center cursor-pointer hover:scale-110 transition-all featHide`}
-          onClick={()=>{router.push("/incharge/feedback")}}
+          onClick={() => {
+            router.push("/incharge/search");
+          }}
         >
-          <p className={`px-1 rounded-md relative py-3 ${
-                  isActive("/incharge/feedback") ? "border" : ""
-                }`}>
+          <p className={`p-1 rounded-md relative`}>
+            <svg
+              fill="#6b7280"
+              className={` w-12 h-8 `}
+              viewBox="0 0 50 50"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M35.66 29.539c1.661-2.739 2.632-5.948 2.632-9.385 0-10.029-8.115-18.15-18.146-18.154-10.022.003-18.146 8.125-18.146 18.152 0 10.018 8.125 18.139 18.152 18.139 3.44 0 6.645-.972 9.384-2.633l12.343 12.342 6.121-6.124-12.34-12.337zm-15.51 1.841c-6.202-.015-11.216-5.027-11.227-11.216.012-6.202 5.027-11.215 11.227-11.229 6.199.016 11.215 5.028 11.228 11.229-.013 6.182-5.031 11.201-11.228 11.216z" />
+            </svg>
+            <p
+              className={`text-black px-2 py-1 rounded-md absolute top-2 -right-20 featH bg-white`}
+            >
+              Search
+            </p>
+            <p
+              className={`text-center text-[12px] ${
+                arima.className
+              } mt-3 uppercase ${
+                isActive("/incharge/search") ? "text-white" : "text-gray-700"
+              }`}
+            >
+              Search
+            </p>{" "}
+          </p>
+        </div>
+        <div
+          className={`flex justify-center py-5 items-center cursor-pointer hover:scale-110 transition-all featHide`}
+          onClick={() => {
+            router.push("/incharge/feedback");
+          }}
+        >
+          <p
+            className={`px-1 flex flex-col items-center rounded-md relative py-3`}
+          >
             <svg
               fill="#6b7280"
               className={`w-12 h-8 `}
@@ -103,7 +97,14 @@ export default function SideNav() {
               <path d="M27.4133467,3.10133815 L32.0133467,18.1013381 C32.2133467,18.7013381 32.8133467,19.0013381 33.4133467,19.0013381 L48.4133467,19.0013381 C49.9133467,19.0013381 50.5133467,21.0013381 49.3133467,21.9013381 L37.1133467,30.9013381 C36.6133467,31.3013381 36.4133467,32.0013381 36.6133467,32.6013381 L42.4133467,48.0013381 C42.8133467,49.4013381 41.3133467,50.6013381 40.1133467,49.7013381 L27.0133467,39.9013381 C26.5133467,39.5013381 25.8133467,39.5013381 25.2133467,39.9013381 L12.0133467,49.7013381 C10.8133467,50.6013381 9.21334668,49.4013381 9.71334668,48.0013381 L15.3133467,32.6013381 C15.5133467,32.0013381 15.3133467,31.3013381 14.8133467,30.9013381 L2.61334668,21.9013381 C1.41334668,21.0013381 2.11334668,19.0013381 3.51334668,19.0013381 L18.5133467,19.0013381 C19.2133467,19.0013381 19.7133467,18.8013381 19.9133467,18.1013381 L24.6133467,3.00133815 C25.0133467,1.60133815 27.0133467,1.70133815 27.4133467,3.10133815 Z M26.0133467,12.8023264 C26,14.1700393 26,33.5426636 26,34.4953918 C26.1865845,34.6476135 28.9331193,36.6890643 34.2396046,40.6197441 C34.9394191,41.144605 35.8141872,40.4447905 35.5809157,39.6283403 L35.5809157,39.6283403 L32.3085327,31.0201416 C31.9597778,30.2501831 32.3085327,29.7487793 32.7398682,29.4849854 L32.7398682,29.4849854 L39.6048489,24.6961622 C40.3046634,24.1713013 39.9547562,23.0049438 39.0799881,23.0049438 L39.0799881,23.0049438 L31.0206299,23.0049438 C30.6707226,23.0049438 29.7518921,22.8880615 29.5025635,21.9888306 L29.5025635,21.9888306 L26.8332347,13.4436151 C26.7175852,13.0388421 26.3602784,12.8204102 26.0133467,12.8023264 Z" />
             </svg>
             <p
-              className={`text-black px-2 py-1 rounded-md absolute top-2 -right-24 featH bg-white`}
+              className={`text-black px-2 py-1 rounded-md absolute top-5 -right-24 featH bg-white`}
+            >
+              Feedback
+            </p>
+            <p
+              className={`text-center mt-3 text-[12px] ${arima.className} ${
+                isActive("/incharge/feedback") ? "text-white" : "text-gray-600"
+              }`}
             >
               Feedback
             </p>
